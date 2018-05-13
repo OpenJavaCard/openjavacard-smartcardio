c3484c4aa22519adedbbd765359dddd548135b3f
Ingo Albrecht <prom@berlin.ccc.de>
2018-05-13 16:35:41 +0200

    Javadoc

--- framework/src/main/java/javax/smartcardio/ATR.java
+++ framework/src/main/java/javax/smartcardio/ATR.java
@@ -2,6 +2,9 @@ package javax.smartcardio;
 
 import java.util.Arrays;
 
+/**
+ * Representation of a smart card answer-to-reset
+ */
 public class ATR {
 
     private final byte[] mBytes;
@@ -9,14 +12,20 @@ public class ATR {
     private int mHistOffset;
     private int mHistLength;
 
+    /**
+     * Main constructor
+     * @param bytes with raw ATR
+     */
     public ATR(byte[] bytes) {
         mBytes = bytes.clone();
     }
 
+    /** @return a copy of the raw ATR */
     public byte[] getBytes() {
         return mBytes.clone();
     }
 
+    /** @return the historical bytes of the ATR */
     public byte[] getHistoricalBytes() {
         byte[] b = new byte[mHistLength];
         System.arraycopy(mBytes, mHistOffset, b, 0, mHistLength);
